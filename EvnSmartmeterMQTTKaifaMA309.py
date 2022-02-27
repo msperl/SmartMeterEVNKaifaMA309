2cce11c22bb1b43719d6b369c6937478fa775bb2
Martin Sperl <kernel@martin.sperl.org>
2022-02-27 15:51:27 +0000

    restructured code and made the code more resilleient to erros as well as translating to english
    Signed-off-by: Martin Sperl <kernel@martin.sperl.org>

--- EvnSmartmeterMQTTKaifaMA309.py
+++ EvnSmartmeterMQTTKaifaMA309.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 from gurux_dlms.GXByteBuffer import GXByteBuffer
 import serial
 import time
@@ -9,141 +11,200 @@ import paho.mqtt.client as mqtt
 from gurux_dlms.GXDLMSTranslator import GXDLMSTranslator
 from gurux_dlms.GXDLMSTranslatorMessage import GXDLMSTranslatorMessage
 from bs4 import BeautifulSoup
+import os
+import traceback
 
-# EVN Schlüssel eingeben zB. "36C66639E48A8CA4D6BC8B282A793BBB"
-evn_schluessel = "dein_Schlüssel"
+#workarround for missing distutils.util
+def str_to_bool(value: any) -> bool:
+    if not value:
+        return False
+    return str(value).lower() in ("y", "yes", "t", "true", "on", "1")
 
-#MQTT Verwenden (True | False)
-useMQTT = False
+class mbus_parser():
 
-#MQTT Broker IP adresse Eingeben ohne Port!
-mqttBroker = "192.168.1.10"
-mqttuser =""
-mqttpasswort = ""
-#Aktuelle Werte auf Console ausgeben (True | False)
-printValue = True
+    mbus_message_length=282
+    mbus_start_pattern=b'\x68\xfa\xfa\x68\x53\xff\x00\x01\x67\xdb\x08'
+    mbus_end_pattern=b'\x16'
 
-#Comport Config/Init
-comport = "/dev/ttyUSB0"
+    def __init__(self, dev, key):
+        self.serial_dev = dev
+        self.key = key
+        self.printRaw = True
+        self.printXml = True
+
+        self.tr = GXDLMSTranslator()
+        self.tr.blockCipherKey = GXByteBuffer(self.key)
+        self.tr.comments = True
+
+        self.ser = serial.Serial(port=self.serial_dev,
+                                 baudrate=2400,
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE
+                                )
+    
+    def readMbusMessage(self):
+        # wait for the start sequence
+        self.ser.read_until(self.mbus_start_pattern)
+    
+        # nw read the complete block 
+        self.data = (self.mbus_start_pattern + self.ser.read(size=self.mbus_message_length - len(self.mbus_start_pattern))).hex()
+
+        if self.printRaw:
+            print("Data received: %s" % self.data, flush=True)
+        
+        if not self.data.endswith(self.mbus_end_pattern.hex()):
+            print("%s: unexpected data trailer - need to restart - %s" % (sys.argv[0], self.data), file=sys.stderr)
+            self.data = None
+    
+        # there is another marker we can check as well - we can also try to match that one...
+        # complete start pattern is: 68FAFA6853FF000167DB08................81F820
+
+        # and there is in principle a CRC checksum, but how that is computed is not documented...
+
+    def parseData(self):
+        self.xml = ""
+        if not self.data:
+            self.xml = None
+            return
+        msg = GXDLMSTranslatorMessage()
+        msg.message = GXByteBuffer(self.data)
+        pdu = GXByteBuffer()
+        self.tr.completePdu = True
+        while self.tr.findNextFrame(msg, pdu):
+            pdu.clear()
+            self.xml += self.tr.messageToXml(msg)
+
+        if self.printXml:
+            print("xml received: %s" % self.xml, flush=True)
+
+        # parse
+        soup = BeautifulSoup(self.xml, 'lxml')
+        results_32 = soup.find_all('uint32')
+        results_16 = soup.find_all('uint16')
+
+        #ActiveEnergy A+ in KiloWattstunden
+        self.kWhP = int(str(results_32)[16:16+8],16)/1000
+
+        #ActiveEnergy A- in KiloWattstunden
+        self.kWhN = int(str(results_32)[52:52+8],16)/1000
 
 
-#MQTT Init
-if useMQTT:
+        #CurrentElectricPower P+ in Watt
+        self.WattP = int(str(results_32)[88:88+8],16)
+
+        #CurrentElectricPower P- in Watt
+        self.WattN = int(str(results_32)[124:124+8],16)
+
+        #Voltage L1 in Volt
+        self.VoltageL1 = int(str(results_16)[16:20],16)/10
+
+        #Voltage L2 in Volt
+        self.VoltageL2 = int(str(results_16)[48:52],16)/10
+
+        #Voltage L3 in Volt
+        self.VoltageL3 = int(str(results_16)[80:84],16)/10
+
+        #Current L1 in Ampere
+        self.CurrentL1 = int(str(results_16)[112:116],16)/100
+
+        #Current L2 in Ampere
+        self.CurrentL2 = int(str(results_16)[144:148],16)/100
+
+        #Current L3 in Ampere
+        self.CurrentL3 = int(str(results_16)[176:180],16)/100
+
+        #PowerFactor
+        self.PowerFactor = int(str(results_16)[208:212],16)/1000
+            
+    def printValues(self):
+        if not self.xml:
+            return
+        print('Voltage L1:  ' + str(self.VoltageL1))
+        print('Voltage L2:  ' + str(self.VoltageL2))
+        print('Voltage L3:  ' + str(self.VoltageL3))
+        print('Current L1:  ' + str(self.CurrentL1))
+        print('Current L2:  ' + str(self.CurrentL2))
+        print('Current L3:  ' + str(self.CurrentL3))
+        print('WattP+:      ' + str(self.WattP))
+        print('WattP-:      ' + str(self.WattN))
+        print('Watt:        ' + str(self.WattP - self.WattN))
+        print('PowerFactor: ' + str(self.PowerFactor))
+        print('kWh+:        ' + str(self.kWhP))
+        print('kWh-:        ' + str(self.kWhN))
+        print('kWh:         ' + str(self.kWhP - self.kWhN))
+        print('==========================================================', flush=True)
+
+    def publishValues(self, client):
+        if not self.xml:
+            return
+        client.publish(mqttTopicPrefix + "/VoltageL1",   self.VoltageL1)
+        client.publish(mqttTopicPrefix + "/VoltageL2",   self.VoltageL2)
+        client.publish(mqttTopicPrefix + "/VoltageL3",   self.VoltageL3)
+        client.publish(mqttTopicPrefix + "/CurrentL1",   self.CurrentL1)
+        client.publish(mqttTopicPrefix + "/CurrentL2",   self.CurrentL2)
+        client.publish(mqttTopicPrefix + "/CurrentL3",   self.CurrentL3)
+        client.publish(mqttTopicPrefix + "/WattP",       self.WattP)
+        client.publish(mqttTopicPrefix + "/WattN",       self.WattN)
+        client.publish(mqttTopicPrefix + "/Watt",        self.WattP - self.WattN)
+        client.publish(mqttTopicPrefix + "/kWhP",        self.kWhP)
+        client.publish(mqttTopicPrefix + "/kWhN",        self.kWhN)
+        client.publish(mqttTopicPrefix + "/kWh",         self.kWhP - self.kWhN)
+        client.publish(mqttTopicPrefix + "/PowerFactor", self.PowerFactor)
+
+# the encryption_key
+encryption_key = os.environ.get("EVN_KEY")
+if not encryption_key:
+    print("%s: environment variable EVN_KEY is not set" % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+comport = os.environ.get("SERIAL_PORT")
+if not comport:
+    print("%s: environment variable SERIAL_PORT is not set" % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+
+try:
+    print("opening Serial", flush=True)
+    mbus = mbus_parser(comport, encryption_key)
+except Exception as e:
+    print("%s: could not connect to serial - %s" % (sys.argv[0], str(e)), file=sys.stderr)
+    sys.exit(2)
+
+# handle print args
+mbus.printRaw = str_to_bool(os.environ.get("PRINT_RAW","False"))
+mbus.printXml = str_to_bool(os.environ.get("PRINT_XML","False"))
+printValues = str_to_bool(os.environ.get("PRINT_DATA","False"))
+    
+#MQTT Broker 
+mqttBroker = os.environ.get("MQTT_HOST")
+if mqttBroker:
     try:
-        client = mqtt.Client("SmartMeter")
-        client.username_pw_set(mqttuser, mqttpasswort)
-        client.connect(mqttBroker, port=1883)
-    except:
-        print("Die Ip Adresse des Brokers ist falsch!")
-        sys.exit()
+        mqttBrokerPort = int(os.environ.get("MQTT_PORT", "1883"))
+        mqttUser = os.environ.get("MQTT_USER", "")
+        mqttPasswort = os.environ.get("MQTT_PASS", "")
+        mqttClientName = os.environ.get("MQTT_CLIENT_NAME", "SmartMeter")
+        mqttTopicPrefix = os.environ.get("MQTT_TOPIC_PREFIX", "Smartmeter")
+        
+        client = mqtt.Client(mqttClientName)
+        client.username_pw_set(mqttUser, mqttPasswort)
+        client.connect(mqttBroker, port=mqttBrokerPort, keepalive=60)
+        client.loop_start()
+    except Exception as e:
+        print("%s: the broker IP/port/user/password is wrong - %s" % (sys.argv[0], str(e)), file=sys.stderr)
+        sys.exit(2)
+else:
+    # force printing if no broker is configured
+    printValues = True
 
-
-
-tr = GXDLMSTranslator()
-tr.blockCipherKey = GXByteBuffer(evn_schluessel)
-tr.comments = True
-ser = serial.Serial( port=comport,
-         baudrate=2400,
-         bytesize=serial.EIGHTBITS,
-         parity=serial.PARITY_NONE,
-)
-
-
+print("Starting main loop", flush=True)
 while 1:
-    daten = ser.read(size=282).hex()
-    print(daten)
-
-    msg = GXDLMSTranslatorMessage()
-    msg.message = GXByteBuffer(daten)
-    xml = ""
-    pdu = GXByteBuffer()
-    tr.completePdu = True
-    while tr.findNextFrame(msg, pdu):
-        pdu.clear()
-        xml += tr.messageToXml(msg)
-
-    soup = BeautifulSoup(xml, 'lxml')
-
-    results_32 = soup.find_all('uint32')
-    results_16 = soup.find_all('uint16')
-    #print(results_16)
-
-    #Wirkenergie A+ in KiloWattstunden
-    WirkenergieP = int(str(results_32)[16:16+8],16)/1000
-
-    #Wirkenergie A- in KiloWattstunden
-    WirkenergieN = int(str(results_32)[52:52+8],16)/1000
-
-
-    #Momentanleistung P+ in Watt
-    MomentanleistungP = int(str(results_32)[88:88+8],16)
-
-    #Momentanleistung P- in Watt
-    MomentanleistungN = int(str(results_32)[124:124+8],16)
-
-    #Spannung L1 in Volt
-    SpannungL1 = int(str(results_16)[16:20],16)/10
-
-    #Spannung L2 in Volt
-    SpannungL2 = int(str(results_16)[48:52],16)/10
-
-    #Spannung L3 in Volt
-    SpannungL3 = int(str(results_16)[80:84],16)/10
-
-
-
-    #Strom L1 in Ampere
-    StromL1 = int(str(results_16)[112:116],16)/100
-
-    #Strom L2 in Ampere
-    StromL2 = int(str(results_16)[144:148],16)/100
-
-    #Strom L3 in Ampere
-    StromL3 = int(str(results_16)[176:180],16)/100
-
-
-    #Leistungsfaktor
-    Leistungsfaktor = int(str(results_16)[208:212],16)/1000
-
-
-
-    if printValue:
-        print('Wirkenergie+: ' + str(WirkenergieP))
-        print('Wirkenergie: ' + str(WirkenergieN))
-        print('MomentanleistungP+: ' + str(MomentanleistungP))
-        print('MomentanleistungP-: ' + str(MomentanleistungN))
-        print('Spannung L1: ' + str(SpannungL1))
-        print('Spannung L2: ' + str(SpannungL2))
-        print('Spannung L3: ' + str(SpannungL3))
-        print('Strom L1: ' + str(StromL1))
-        print('Strom L2: ' + str(StromL2))
-        print('Strom L3: ' + str(StromL3))
-        print('Leistungsfaktor: ' + str(Leistungsfaktor))
-        print('Momentanleistung: ' + str(MomentanleistungP-MomentanleistungN))
-        print()
-        print()
-
-
-    #MQTT
-    if useMQTT:
-        client.publish("Smartmeter/WirkenergieP", WirkenergieP)
-        client.publish("Smartmeter/WirkenergieN", WirkenergieN)
-        client.publish("Smartmeter/MomentanleistungP", MomentanleistungP)
-        client.publish("Smartmeter/MomentanleistungN", MomentanleistungN)
-        client.publish("Smartmeter/Momentanleistung", MomentanleistungP - MomentanleistungN)
-        client.publish("Smartmeter/SpannungL1", SpannungL1)
-        client.publish("Smartmeter/SpannungL2", SpannungL2)
-        client.publish("Smartmeter/SpannungL3", SpannungL3)
-        client.publish("Smartmeter/StromL1", StromL1)
-        client.publish("Smartmeter/StromL2", StromL2)
-        client.publish("Smartmeter/StromL3", StromL3)
-        client.publish("Smartmeter/Leistungsfaktor", Leistungsfaktor)
-    #except BaseException as err:
-    #   print("Fehler beim Synchronisieren. Programm bitte ein weiteres mal Starten.")
-    #   print()
-    #   print("Fehler: ", format(err))
-
-    #   sys.exit()
-
-
+    try:
+        mbus.readMbusMessage()
+        mbus.parseData()
+        if printValues:
+            mbus.printValues()
+        if mqttBroker:
+            mbus.publishValues(client)
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print("%s: Error handling the main loop: %s" % (sys.argv[0], format(e)), file=sys.stderr)
+        traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stderr)
 
