6270df94f9ccdfe43fe54c6607b63d188a9f8228
Martin Sperl <kernel@martin.sperl.org>
2022-12-26 12:47:52 +0000

    start using home_assistant_mqtt.py as a module
    Signed-off-by: Martin Sperl <kernel@martin.sperl.org>

--- EvnSmartmeterMQTTKaifaMA309.py
+++ EvnSmartmeterMQTTKaifaMA309.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python3
 
+# include script directory to include the homeassistant helper
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(dir_path)
+from home_assistant_mqtt import home_assistant_mqtt
+
+# all the other things to include
 from gurux_dlms.GXByteBuffer import GXByteBuffer
 import serial
 import time
@@ -11,7 +19,6 @@ import paho.mqtt.client as mqtt
 from gurux_dlms.GXDLMSTranslator import GXDLMSTranslator
 from gurux_dlms.GXDLMSTranslatorMessage import GXDLMSTranslatorMessage
 from bs4 import BeautifulSoup
-import os
 import traceback
 import json
 
@@ -20,77 +27,6 @@ def str_to_bool(value: any) -> bool:
     if not value:
         return False
     return str(value).lower() in ("y", "yes", "t", "true", "on", "1")
-
-class home_assistant_mqtt():
-
-    valid_configs = {
-        "name": None,
-        "unique_id": None,
-        "unit_of_measurement": None,
-        "state_class": "measurement",
-        "icon": None,
-        "state_topic": None,
-        "value_template": None,
-        "json_attributes_topic": None,
-        "device": None
-    }
-
-    def publish(client, configs, prefix, state_topic, config_topic, publish_config):
-        # enrich the config settings and prepare the individual state topics
-        state_topic_configs = {}
-        for k,v in configs.items():
-            # enrich config
-            if not "label" in v:
-                v["label"] = k
-            if not "object_id" in v:
-                v["object_id"] = prefix
-            if not "name" in v:
-                v["name"] = v["object_id"] + " - " + k
-            if not "state_class" in v:
-                v["state_class"] = "measurement"
-            if not "state_topic" in v:
-                v["state_topic"] = state_topic % k
-            if not "unique_id" in v:
-                v["unique_id"] = prefix + "-" + k
-            # assign defaults
-            for kd, vd in home_assistant_mqtt.valid_configs.items():
-                if (kd not in v) and (vd != None):
-                    v[kd] = vd
-            # assign value
-            if v["value"] != None:
-                if v["state_topic"] not in state_topic_configs:
-                    state_topic_configs[v["state_topic"]] = []
-                state_topic_configs[v["state_topic"]].append(v)
-        # and publish all the states
-        for k,v in state_topic_configs.items():
-            if len(v) == 1:
-                value = v[0].get("value", None)
-            else:
-                value = {}
-                for i in v:
-                    vv = i.get("value", None)
-                    if vv != None:
-                        value[i[label]] = vv
-                    i["value_template"] = "{{ value_json." + i["label"] + " }}"
-                if len(value) > 0:
-                    value = json.dumps(value, indent=4)
-                else:
-                    value = None
-            if value != None:
-                client.publish(k, value)
-        # publish config when requested
-        if publish_config:
-            for k,v in configs.items():
-                # generate a valid final config
-                cfg = {}
-                for key in home_assistant_mqtt.valid_configs.keys():
-                    if key in v:
-                        cfg[key] = v[key]
-                # render the topic
-                c_topic = config_topic % v["label"]
-                c_json = json.dumps(cfg, indent=4)
-                # and publish it
-                client.publish(c_topic, c_json)
 
 class mbus_parser():
 
---
+++ home_assistant_mqtt.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+
+import time
+import sys
+import string
+import paho.mqtt.client as mqtt
+import os
+import traceback
+import json
+
+class home_assistant_mqtt():
+
+    valid_configs = {
+        "name": None,
+        "unique_id": None,
+        "unit_of_measurement": None,
+        "state_class": "measurement",
+        "icon": None,
+        "state_topic": None,
+        "value_template": None,
+        "json_attributes_topic": None,
+        "device": None
+    }
+
+    def publish(client, configs, prefix, state_topic, config_topic, publish_config):
+        # enrich the config settings and prepare the individual state topics
+        state_topic_configs = {}
+        for k,v in configs.items():
+            # enrich config
+            if not "label" in v:
+                v["label"] = k
+            if not "object_id" in v:
+                v["object_id"] = prefix
+            if not "name" in v:
+                v["name"] = v["object_id"] + " - " + k
+            if not "state_class" in v:
+                v["state_class"] = "measurement"
+            if not "state_topic" in v:
+                v["state_topic"] = state_topic % k
+            if not "unique_id" in v:
+                v["unique_id"] = prefix + "-" + k
+            # assign defaults
+            for kd, vd in home_assistant_mqtt.valid_configs.items():
+                if (kd not in v) and (vd != None):
+                    v[kd] = vd
+            # assign value
+            if v["value"] != None:
+                if v["state_topic"] not in state_topic_configs:
+                    state_topic_configs[v["state_topic"]] = []
+                state_topic_configs[v["state_topic"]].append(v)
+        # and publish all the states
+        for k,v in state_topic_configs.items():
+            if len(v) == 1:
+                value = v[0].get("value", None)
+            else:
+                value = {}
+                for i in v:
+                    vv = i.get("value", None)
+                    if vv != None:
+                        value[i[label]] = vv
+                    i["value_template"] = "{{ value_json." + i["label"] + " }}"
+                if len(value) > 0:
+                    value = json.dumps(value, indent=4)
+                else:
+                    value = None
+            if value != None:
+                client.publish(k, value)
+        # publish config when requested
+        if publish_config:
+            for k,v in configs.items():
+                # generate a valid final config
+                cfg = {}
+                for key in home_assistant_mqtt.valid_configs.keys():
+                    if key in v:
+                        cfg[key] = v[key]
+                # render the topic
+                c_topic = config_topic % v["label"]
+                c_json = json.dumps(cfg, indent=4)
+                # and publish it
+                client.publish(c_topic, c_json)
